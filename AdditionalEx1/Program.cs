0c36890b5da4317b307f721ab5e512172611e11a
Dasha <dnazarkina@ya.ru>
2022-11-15 19:49:27 +0300

    added changes inside AddEx1

--- AdditionalEx1/Program.cs
+++ AdditionalEx1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//*В переменной string есть секретное сообщение, во второй есть пароль. 
+//Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.
+
+
+for (int entries = 0; entries < 3; entries++)
+{
+    Console.Write(" Please enter your password: ");
+    String? Password = Console.ReadLine(); 
+    if (Password == "Dasha")
+    {
+        Console.WriteLine("What a lovely day today =)");
+        break;
+    }
+    else
+    {
+        Console.WriteLine(" Password is incorrect. Please try again ");
+    }
+}
+
+
+
+
+    
+
+
