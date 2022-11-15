c7e42da2288e6de8394c63a5b3f5a80990b37133
Dasha <dnazarkina@ya.ru>
2022-11-15 21:19:00 +0300

    added more info to AddEx2

--- AdditionalEx2/Program.cs
+++ AdditionalEx2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+
+Console.Write(" To stop the program please use EXIT: ");
+String? Password = Console.ReadLine(); 
+int i = 0;
+
+for (; ; )
+{
+    i++;
+    Console.WriteLine(i);
+    if (Password == "exit") break;
+    if (i == 15) break;
+}
+Console.ReadLine();
