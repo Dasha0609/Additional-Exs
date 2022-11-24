22ccabcf582342be4d4b6d2e96b6a36e512b166c
Dasha <dnazarkina@ya.ru>
2022-11-24 19:54:07 +0300

    added 4 more commands

--- AdditionalEx3/Program.cs
+++ AdditionalEx3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Добавить к программе, которая заканчивается, когда пишем exit еще 4 команды (их можно придумать самому). Например:
+SetName – Установить имя
+Help – вывести список команд
+SetPassword – Установить пароль
+Exit – выход
+WriteName – вывести имя после ввода пароля*/
+
+
+string userString = "";
+do
+{
+    Console.Write("Please enter your name: ");
+    userString = Console.ReadLine();  
+    Console.WriteLine("Your infomation is: " + userString);
+
+} while (userString != "exit");
