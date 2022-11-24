7bb79a808c1ae1728844c1b2afb05d8e8fc61d60
Dasha <dnazarkina@ya.ru>
2022-11-24 19:56:18 +0300

    added 4 more commands

--- AdditionalEx2/Program.cs
+++ AdditionalEx2/Program.cs
@@ -1,4 +1,9 @@
-﻿// Есть программа с бесконечным циклом. Когда пользователь вводит exit программа заканчивается
+﻿/*Добавить к программе, которая заканчивается, когда пишем exit еще 4 команды (их можно придумать самому). Например:
+SetName – Установить имя
+Help – вывести список команд
+SetPassword – Установить пароль
+Exit – выход
+WriteName – вывести имя после ввода пароля*/
 
 
 while (true)
@@ -8,6 +13,41 @@ while (true)
    if (repeat == "exit")
    {
     break;
+   }
+   if (repeat == "name")
+   {
+      Console.WriteLine("Please enter your name here: ");
+      string name = Console.ReadLine();
+      Console.WriteLine("Hello " + name);
+      Console.WriteLine("Would you like to set password: Yes or No");
+      string pas = Console.ReadLine();
+      
+      if (pas == "yes")
+      {
+         Console.WriteLine("Pls enter your password here: ");
+         string password = Console.ReadLine();
+         Console.WriteLine("You password is " + password);
+      }
+   }
+   if (repeat == "age")
+   {
+      Console.WriteLine("Please enter your age here: ");
+      string input = Console.ReadLine();
+      int age = Convert.ToInt32(input);
+
+      if (age < 18)
+      {
+         Console.Write("You're too young for alcohol");
+      }
+      else
+      {
+         Console.WriteLine("Enjoy you time");
+      }
+
+   }
+   if (repeat == "help")
+   {
+      Console.WriteLine("used commands: age, name, help and exit");
    }
 }
 
